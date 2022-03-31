f3d92aa79a7a04189df4124e251560975a237176
Jeong Yoon <dlwjddbs14@naver.com>
2022-03-31 19:09:53 +0900

    🌟 :: 프로젝트 세팅

---
+++ src/main/java/com/example/trapezius/TrapeziusApplication.java
@@ -0,0 +1,13 @@
+package com.example.trapezius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrapeziusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrapeziusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trapezius/TrapeziusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trapezius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrapeziusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
