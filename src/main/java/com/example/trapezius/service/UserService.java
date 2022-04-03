08955d2cc29221c0c0ed4e44c1bf83fba131f276
Jeong Yoon <dlwjddbs14@naver.com>
2022-04-03 11:22:17 +0900

    ♻ :: @Required 지우고 직접 빈 주입해줌 이렇게 하는거 맞지?

--- src/main/java/com/example/trapezius/service/UserService.java
+++ src/main/java/com/example/trapezius/service/UserService.java
@@ -4,14 +4,16 @@ import com.example.trapezius.api.dto.LoginRequest;
 import com.example.trapezius.api.dto.SignupRequest;
 import com.example.trapezius.entity.User;
 import com.example.trapezius.entity.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public String signup(SignupRequest request) {
         userRepository.save(new User(request.getId(), request.getPassword(), request.getName()));
@@ -19,7 +21,8 @@ public class UserService {
     }
 
     public String login(LoginRequest request) {
-        if (userRepository.findById(request.getId()).get().getPassword() == request.getPassword());
+        if (userRepository.findById(request.getId()).get().getPassword()
+                .equals(request.getPassword()));
         return "로그인 성공";
     }
 }
