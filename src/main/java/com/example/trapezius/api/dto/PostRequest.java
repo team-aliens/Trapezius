f30debdbe44ff582a7396765e467990307c83b9f
Jeong Yoon <dlwjddbs14@naver.com>
2022-04-05 22:55:16 +0900

    🌨 :: 오늘 일을 계기로 준비 확실하게 ㅜㅜ

---
+++ src/main/java/com/example/trapezius/api/dto/PostRequest.java
@@ -0,0 +1,12 @@
+package com.example.trapezius.api.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostRequest {
+
+    private String title;
+    private String content;
+}
--- src/main/java/com/example/trapezius/hard/entity/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.trapezius.entity.repository;
-//
-//import com.example.trapezius.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, String> {
-//}
